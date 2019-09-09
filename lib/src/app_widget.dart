8c4402e4df66ef5f4d9eb2eb649dd9b8da98982c
Jacob  Moura <jacobmoura@Jacobs-iMac.local>
2019-09-08 22:08:39 -0300

    added image and apk files

--- lib/src/app_widget.dart
+++ lib/src/app_widget.dart
@@ -5,6 +5,7 @@ class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Slidy',
       theme: ThemeData(
         primaryColor: Color(0xFFF0DA50),
